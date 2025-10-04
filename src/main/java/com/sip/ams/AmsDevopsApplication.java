4ae70cfc0813c6b0b4d1a35d24d593bc4bfae3e7
taher.aymen <taher.aymen@gmail.com>
2025-10-04 17:23:03 +0200

    first commit

---
+++ src/main/java/com/sip/ams/AmsDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsDevopsApplication.class, args);
+		System.out.println("Hello Devops");
+	}
+
+}
